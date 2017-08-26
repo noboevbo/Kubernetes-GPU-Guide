97ff816e144c69194b460f1d68f15fa911a13595
Dennis Ludl <Dennis.Ludl@reutlingen-university.de>
2017-08-26 20:00:49 +0200

    Caffe YAML

---
+++ deployments/runcaffe.sh
@@ -0,0 +1,34 @@
+kubectl run -i --tty --rm caffetest --image=kaixhin/cuda-caffe:8.0 --restart=Never --overrides='
+{
+  "apiVersion": "extensions/v1beta1",
+  "kind": "Deployment",
+  "spec": {
+    "template": {
+      "spec": {
+        "containers": [
+          {
+            "name": "caffetest",
+            "image": "kaixhin/cuda-caffe:8.0",
+            "args": [
+              "bash"
+            ],
+            "stdin": true,
+            "stdinOnce": true,
+            "tty": true,
+            "volumeMounts": [{
+              "mountPath": "/usr/local/nvidia",
+              "name": "nvidia-driver"
+            }]
+          }
+        ],
+        "volumes": [{
+          "name":"nvidia-driver",
+          "hostPath":{
+            "path":"/var/lib/nvidia-docker/volumes/nvidia_driver/375.82"
+          }
+        }]
+      }
+    }
+  }
+}
+' -- bash
